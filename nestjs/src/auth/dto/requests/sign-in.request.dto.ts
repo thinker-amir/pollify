f091fde25c5a46533053808eabfdd394031d6922
thinker <thinker.amir@gmail.com>
2024-04-17 00:35:16 +0330

    Add swagger examples in SignInRequestDto

--- nestjs/src/auth/dto/requests/sign-in.request.dto.ts
+++ nestjs/src/auth/dto/requests/sign-in.request.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length, MinLength } from 'class-validator';
 
 export class SignInRequestDto {
+  @ApiProperty({ example: 'eshpitegah' })
   @IsString()
   @Length(2, 20)
   username: string;
 
+  @ApiProperty({ example: 'dummy-secret' })
   @IsString()
   @MinLength(8)
   password: string;
